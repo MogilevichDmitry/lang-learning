43b3fc0a90c25e403c3c15b7eef7c31417006adf
DESKTOP-8CLM53A\User <dimovitch.sergey@gmail.com>
2022-06-28 11:54:35 +0300

    clean up project

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Lang-learning</title>
+        <meta name="description" content="Lang-learning - Обучалка" />
+      </Head>
+      <div>INDEX PAGE</div>
+    </>
+  );
+};
+
+export default Home;
